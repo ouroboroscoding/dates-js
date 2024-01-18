e4d67d46b1964d07353d976290257c2f9f202bcd
Ouroboros Coding Inc <chris@ouroboroscoding.com>
2024-01-18 17:26:36 -0500

    Hotfix 1.0.1 - Fixed issue with timestamp() returning a float instead of an int.

--- index.ts
+++ index.ts
@@ -532,14 +532,14 @@ export function timestamp(d?: validDate, utc: boolean = true): number {
 
 	// If no date was passed, get the current time
 	if(d === undefined) {
-		return (Date.now() / 1000);
+		return Math.floor(Date.now() / 1000);
 	}
 
 	// Else, convert the date
 	d = toDate(d, utc);
 
 	// Return the timestamp of the date passed
-	return (d.getTime() / 1000);
+	return Math.floor(d.getTime() / 1000);
 }
 
 // Default export
